994d702fe772295186c0ac50e13fda7f835306e8
Naervern <naervern@gmail.com>
2022-07-29 01:22:45 -0300

    Some changes required for the tasks
    Skills Network Platform is extremely sluggish now

--- final_project/server.py
+++ final_project/server.py
@@ -3,22 +3,25 @@ from flask import Flask, render_template, request
 import json
 
 app = Flask("Web Translator")
+logged = []
 
 @app.route("/englishToFrench")
-def englishToFrench():
+def english_to_french():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    translated = translator.english_to_french(textToTranslate)
+    logged.append({"mode": "en-fr", "input": textToTranslate, "output": translated})
+    return "%s translated into %s" %(textToTranslate, translated)
 
 @app.route("/frenchToEnglish")
-def frenchToEnglish():
+def french_to_english():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    translated = translator.french_to_english(textToTranslate)
+    logged.append({"mode": "fr-en", "input": textToTranslate, "output": translated})
+    return "%s translated into %s" %(textToTranslate, translated)
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return json.dumps(logged)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
